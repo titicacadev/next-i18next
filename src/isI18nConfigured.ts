d33d19d0730f4194528b9ac0eef64d42405a7a38
jaehyeon48 <imjaehyeon48@gmail.com>
2023-01-05 11:32:34 +0900

    CD가 돌아갈때만 국제화 설정 여부 체크

--- src/isI18nConfigured.ts
+++ src/isI18nConfigured.ts
@@ -1,6 +1,6 @@
 
 /* eslint-disable no-console */
-if (typeof window === 'undefined') {
+if (typeof window === 'undefined' && process.env.TAG_NAME?.includes('release-')) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   import(`${process.cwd()}/i18n.d.ts`)
