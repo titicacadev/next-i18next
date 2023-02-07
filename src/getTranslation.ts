6c52a336f4d577145636d304e2bd26ecbcf6e54b
Heeryong Kang <drakang4@gmail.com>
2023-02-07 12:42:51 +0900

    chore: getTranslation generic 수정

--- src/getTranslation.ts
+++ src/getTranslation.ts
@@ -1,4 +1,4 @@
-import { globalI18n } from './appWithTranslation'
+import { globalI18n } from './appWithTranslation';
 
 /**
  * useTranslation 훅을 사용할 수 없는 일반 함수에서
@@ -7,10 +7,12 @@ import { globalI18n } from './appWithTranslation'
  * TF인 경우 common-web, 그 외 local
  */
 // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-export function getTranslation<T extends 'common-web' | 'common-admin' | 'local'>(ns: T) {
+export function getTranslation<
+  T extends 'common-web' | 'common-admin' | 'local',
+>(ns: T) {
   if (!globalI18n) {
-    throw new Error('i18n 인스턴스가 초기화되지 않았습니다.')
+    throw new Error('i18n 인스턴스가 초기화되지 않았습니다.');
   }
 
-  return globalI18n.getFixedT(null, ns)
+  return globalI18n.getFixedT<T, undefined>(null, ns);
 }
